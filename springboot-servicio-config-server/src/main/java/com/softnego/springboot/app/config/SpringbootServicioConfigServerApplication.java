78a406bc2ce5852b3eb411cf24ad85d9c80f889b
xlcrazy503lx@gmail.com <UCbrowser16>
2020-01-10 15:18:27 -0600

    config server

--- springboot-servicio-config-server/src/main/java/com/softnego/springboot/app/config/SpringbootServicioConfigServerApplication.java
+++ springboot-servicio-config-server/src/main/java/com/softnego/springboot/app/config/SpringbootServicioConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.softnego.springboot.app.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringbootServicioConfigServerApplication {
 
--- springboot-servicio-item/src/main/java/com/softnego/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/softnego/springboot/app/item/controllers/ItemController.java
@@ -6,19 +6,29 @@ import com.softnego.springboot.app.item.models.Producto;
 import com.softnego.springboot.app.item.models.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@RefreshScope
 @RestController
 public class ItemController {
 
     @Autowired
     @Qualifier("serviceFeign")
     private ItemService itemService;
+
+    @Value("${configuracion.texto}")
+    private String texto;
 
     @GetMapping("/listar")
     public List<Item> listar(){
@@ -39,6 +49,14 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return item;
+    }
+
+    @GetMapping("/config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+        Map<String,String> json = new HashMap<>();
+        json.put("texto",texto);
+        json.put("puerto",puerto);
+        return new ResponseEntity<Map<String,String>>(json, HttpStatus.OK);
     }
 
 }
